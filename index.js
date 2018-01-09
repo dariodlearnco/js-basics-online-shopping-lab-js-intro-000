84b9a14c715c99b9fd2e2672f33b381a024dff01
Dario Dorella <dario.dorella@gmail.com>
2018-01-09 21:40:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
 
     var cart=getCart()
-    
+
     if ( !cardNumber ) {
         console.log('Sorry, we don\'t have a credit card on file for you.')
         return
