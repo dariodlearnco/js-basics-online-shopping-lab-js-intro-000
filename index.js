160246bf360704447abb64bbb0eb491c8f32d736
Dario Dorella <dario.dorella@gmail.com>
2018-01-09 21:40:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -111,14 +111,14 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
 
+    var cart=getCart()
+    
     if ( !cardNumber ) {
         console.log('Sorry, we don\'t have a credit card on file for you.')
         return
     }
-
     console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`)
 
-    var cart=getCart()
-    cart=[]
+    cart.splice(0,cart.length)
 
 }
