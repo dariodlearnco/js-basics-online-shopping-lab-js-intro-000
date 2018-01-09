769d8094f20958ea9356033f5aad52d1bd122845
Dario Dorella <dario.dorella@gmail.com>
2018-01-09 21:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -114,9 +114,10 @@ function placeOrder(cardNumber) {
 
     if ( !cardNumber ) {
         console.log('Sorry, we don\'t have a credit card on file for you.')
+        return
     }
 
-    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`)
+    console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`)
 
     cart=getCart()
     cart=[]
