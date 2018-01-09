b3936f3e58843ab1fdd4186738b6b1d068b33893
Dario Dorella <dario.dorella@gmail.com>
2018-01-09 21:41:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -119,6 +119,7 @@ function placeOrder(cardNumber) {
     }
     console.log(`Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`)
 
-    cart.splice(0,cart.length)
+    // cart.splice(0,cart.length)
+    cart=[]
 
 }
